96bdda22cda92abc897fc32bcc78bd1a5daa2ba3
Jogis <ronipihajoki@proton.me>
2025-09-06 20:32:03 +0300

    Harj2
    Harj2 oikein

--- LoopsAdvanced/src/App.java
+++ LoopsAdvanced/src/App.java
@@ -2,16 +2,38 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        int l1 = 0;
-        int l2 = 0;
+     
+     //Harjoitus 1
+//         int l1 = 0;
+//         int l2 = 0;
 
-        System.out.println("First Number?");
-        l1 = Integer.parseInt(in.nextLine());
-        System.out.println("Last Number?");
-        l2 = Integer.parseInt(in.nextLine());
-    do{
-        System.out.println(l1);
-        l1++;
-    } while (l1 <= l2);
-}
+//         System.out.println("First Number?");
+//         l1 = Integer.parseInt(in.nextLine());
+//         System.out.println("Last Number?");
+//         l2 = Integer.parseInt(in.nextLine());
+//     do{
+//         System.out.println(l1);
+//         l1++;
+//     } while (l1 <= l2);
+
+//Harjoitus2
+        String vastaus = "Emma";
+        String arvaus = "";
+        int i = 0;
+
+           do {
+            System.out.println("Guess my name (type stop to exit)");
+            arvaus = in.nextLine();
+            if (arvaus.equals("stop")){
+                break;
+            }
+            i++;
+           } while (!arvaus.equals(vastaus));
+           if(arvaus.equals(vastaus)){
+            System.out.println("Congratulations!");
+           }
+    
+           System.out.println("You guessed " + i + " times.");
+            
+    }
 }
